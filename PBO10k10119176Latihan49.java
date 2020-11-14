502699c8ec8b320e7cdca1c6885fb50c1b8481b7
Muhammad Fauzan Fadhlulbarr <52595452+muhammadfauzanfadhlulbarr@users.noreply.github.com>
2020-11-14 13:58:18 +0700

    Update PBO10k10119176Latihan49.java

--- PBO10k10119176Latihan49.java
+++ PBO10k10119176Latihan49.java
@@ -1,3 +1,20 @@
+/**
+ * Latihan49
+ * Nama     : Muhammad Fauzan Fadhlulbarr
+ * Kelas    : PBO10K
+ * NIM      : 10119176
+ * Program  : Biaya Emas Kawin
+ */
 public class PBO10k10119176Latihan49 {
-    
+    public static void main(String[] args) {
+        EmasKawin masKawin = new EmasKawin();
+
+        masKawin.setEMAS(15.7f);
+        masKawin.setHarga(570000.0f);
+
+        float EMAS = masKawin.getEMAS();
+        float Harga = masKawin.getHarga();
+
+        masKawin.TotalBayar(EMAS, Harga);
+    }
 }
